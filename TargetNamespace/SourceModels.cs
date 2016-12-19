ea3ce60b8767f7ea4d6f521d7125010444b5422c
Igor Soloydenko <soloydenko@gmail.com>
2016-12-18 21:19:25 -0800

    Initial commit -- work in progress

---
+++ TargetNamespace/SourceModels.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace TsModelGen.TargetNamespace
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+
+        public int Age;
+
+        public List<Address> Addresses { get; set; }
+
+        public Address DefaultAddress;
+    }
+
+    public class Employee : Person
+    {
+        public bool IsSubscribedToNews;
+        public decimal Salary { get; set; }
+    }
+
+    public class Address
+    {
+        public string Street { get; set; }
+    }
+}
