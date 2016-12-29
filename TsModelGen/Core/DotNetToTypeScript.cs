c3365a7e7e60b84b40a83a817d6a6c96ca785fc1
Igor Soloydenko <soloydenko@gmail.com>
2016-12-28 17:16:25 -0800

    Move TypeScriptExpressionClass to implementation

--- TsModelGen/Core/DotNetToTypeScript.cs
+++ TsModelGen/Core/DotNetToTypeScript.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Text;
+using TsModelGen.Core.Targets;
 
 namespace TsModelGen.Core
 {
--- TsModelGen/Core/Targets/TranslationContextBuilder.cs
+++ TsModelGen/Core/Targets/TranslationContextBuilder.cs
@@ -380,4 +380,34 @@ namespace TsModelGen.Core.Targets
 
         public TranslatedTypeMetadata TranslatedTypeMetadata { get; } = new TranslatedTypeMetadata();
     }
+
+    // TODO Use this
+    public static class TypeScriptExpression
+    {
+        public static string ClassNameExpression(string generatedTypeName)
+        {
+            return $"export class {generatedTypeName} ";
+        }
+
+        public static string InheritedTypeExpression(TypeInfo parentType)
+        {
+            var generatedParentTypeName = GeneratedType.Name(parentType.BaseType.Name);
+            return $"extends {generatedParentTypeName} ";
+        }
+
+        public static string StartClassBodyExpression()
+        {
+            return "{";
+        }
+
+        public static string MemberDefinitionExpression(string memberName, string memberType, string sourceType)
+        {
+            return $"  public {memberName}: {memberType}; // {sourceType}";
+        }
+
+        public static string EndClassBodyExpression()
+        {
+            return "}";
+        }
+    }
 }
