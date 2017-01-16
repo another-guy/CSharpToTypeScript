d576da496699011a800af826e70ae62813dbe865
Igor Soloydenko <soloydenko@gmail.com>
2017-01-16 00:39:03 -0800

    Add a note to sample commands file

--- CSharpToTypeScript.Tests/SampleFiles/relative-path-test-commands.bash
+++ CSharpToTypeScript.Tests/SampleFiles/relative-path-test-commands.bash
@@ -1,4 +1,9 @@
-﻿cd /c/GitHub/CSharpToTypeScript/CSharpToTypeScript.Tests; \
+﻿# These commands assume:
+#    sample.debug.cfg.json uses /c/new/ as an output directory
+#    /c/GitHub/ is the root for the solution directory
+
+rm -rf /c/new/; \
+cd /c/GitHub/CSharpToTypeScript/CSharpToTypeScript.Tests; \
 dotnet test; \
 cd /c/GitHub/CSharpToTypeScript/CSharpToTypeScript; \
 dotnet run -- -c ../CSharpToTypeScript.Tests/SampleFiles/sample.debug.cfg.json; \
